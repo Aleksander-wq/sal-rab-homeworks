69a2c2e8af1f1e76a57cf512fed6208ce45e5c91
unknown <alex.ru01@mail.ru>
2021-06-07 13:19:14 +0300

    Task Final Done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,24 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let requestData = {data:{client:{}, order: {}, goods: []}};
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+
+    for (let i = 0; i < countOfGoods; i ++) {
+        requestData.data.goods.push({title:goods[i].title, count:goods[i].count});
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    //data.goods = goods.title + goods.count;
+    
+    requestData.data.client = name + ' ' + phone;
+    //"ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53"
+    requestData.data.order.address = "ул. " + address.street + ", " + "дом " + address.house + ", " + address.entrance + " подъезд" + ", " + address.floor + " этаж" + ", " + "кв " + address.flat;
+    requestData.data.order.sum = sum;
+    
+    //data.client = 'Иван';
+    //name + phone + address + data.goods + 
+    //data = {client:data.client, order:data.order, goods:data.goods};
+    //let jsonData = JSON.stringify(requestData);
+    //console.log(jsonData);
+    return JSON.stringify(requestData);
 }
